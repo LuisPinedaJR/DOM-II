75858ce1009c7c2b8f8dddadeb016c73ad5875d8
Luis Pineda <lspnd00@gmail.com>
2020-02-17 19:41:53 -0500

    finished assignment could not get to stretch goal but did check out ?GSAP site

--- js/index.js
+++ js/index.js
@@ -24,12 +24,56 @@ const keyP = document.addEventListener('keydown', () => {
   alert('You pressed a key')
 })
 
-const wheeling = document
-  .querySelectorAll('.text-content')
-  .addEventListener('onwheel', () => {
-    let scale = 1
-    event.preventDefault()
-    scale += event.deltaY * -0.01
-    scale = Math.min(Math.max(0.125, scale), 4)
-    el.style.transform = `scale(${scale})`
-  })
+const button1 = document.querySelector('#btn-one')
+button1.addEventListener('click', function(event) {
+  event.target.style.color = 'black'
+  event.stopPropagation()
+})
+
+const divClick = document.querySelector('#div-border')
+divClick.addEventListener('click', function(event) {
+  event.currentTarget.style.border = '1px solid red'
+})
+
+const button3 = document.querySelector('#btn-three')
+button3.addEventListener('dblclick', function(event) {
+  event.target.style.borderColor = 'red'
+})
+
+const img1 = document.querySelector('#img-one')
+img1.addEventListener('mouseenter', function(event) {
+  event.target.style.border = '2px dashed green'
+})
+
+img1.addEventListener('mouseleave', function(event) {
+  event.target.style.border = 'none'
+})
+
+const img4 = document.querySelector('#img-four')
+img4.addEventListener('mousedown', function(event) {
+  event.target.style.opacity = '0'
+})
+
+img4.addEventListener('mouseup', function(event) {
+  event.target.style.opacity = '1'
+})
+
+const p = document.querySelector('p')
+p.addEventListener('copy', function(event) {
+  alert('copying')
+})
+
+const img2 = document.querySelector('#img-two')
+img2.addEventListener('contextmenu', function(event) {
+  event.target.style.border = '2px solid yellow'
+})
+
+img2.addEventListener('mouseout', function(event) {
+  event.target.style.border = 'none'
+})
+
+const navStop = document.querySelector('nav')
+navStop.addEventListener('click', function(event) {
+  event.preventDefault()
+  alert(`You can't click`)
+})
